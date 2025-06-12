6440f562de84ee015539f263b0c2c7c114bfd543
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2025-06-12 12:02:42 -0300

    adicionando screenshot

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ https://documentation.botcity.dev/tutorials/custom-automations/python-custom/
 from botcity.maestro import * #type: ignore
 import traceback
 from patrimar_dependencies.gemini_ia import ErrorIA
+from patrimar_dependencies.screenshot import screenshot
 from Entities.file_manipulate import FileManipulate
 from Entities.start_report import StartReport
 from datetime import datetime
@@ -116,7 +117,7 @@ if __name__ == '__main__':
             except Exception as e:
                 maestro.error(task_id=int(execution.task_id), exception=e)
 
-            maestro.error(task_id=int(execution.task_id), exception=error, tags={"IA Response": ia_response})
+            maestro.error(task_id=int(execution.task_id), exception=error, screenshot=screenshot(), tags={"IA Response": ia_response})
 
         sleep(5 * 60)
         
